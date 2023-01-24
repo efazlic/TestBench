8671b72294ef0177fad0b5b0f73d0d62bd472b1d
edinf <edin.fazlic@endava.com>
2023-01-24 14:42:48 +0100

    listener fix

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,6 +1,5 @@
 package listeners;
 
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import utils.LoggerUtils;
@@ -10,20 +9,6 @@ public class TestListener extends LoggerUtils implements ITestListener {
 
     private static boolean bUpdateJira = false;
     private static final boolean bListenerTakeScreenshot = PropertiesUtils.getTakeScreenshot();
-
-    @Override
-    public void onTestStart(ITestContext context) {
-        String sSuiteName = context.getSuite().getName();
-        log.info("[SUITE STARTED] " + sSuiteName);
-        context.setAttribute("listenerTakeScreenshot", bListenerTakeScreenshot);
-        bUpdateJira = getUpdateJira(context);
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-        String sSuiteName = context.getSuite().getName();
-        log.info("[SUITE FINISHED] " + sSuiteName);
-    }
 
     @Override
     public void onTestStart(ITestResult result) {
@@ -48,6 +33,4 @@ public class TestListener extends LoggerUtils implements ITestListener {
         String sTestName = result.getTestClass().getName();
         log.info("[TEST SKIPPED] " + sTestName);
     }
-
-
 }
